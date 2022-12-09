1d788cd045e5b8bc61f4a2e629d6fab2c91af4bb
Marcos <marcosrvlzero69@gmail.com>
2022-12-09 09:22:13 -0500

    practica

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const helmet=require('helmet');
+const helmet = require('helmet');
 const app = express();
 const port = 3001;
 
@@ -11,7 +11,7 @@ const db = require('./queries');
 //middlewares
 app.use(helmet());
 app.use(morgan("common"));
-app.use(cors({origin:['localhost:*','192.168.1.69:*']}));
+app.use(cors({ origin: ['localhost:*', '192.168.1.69:*'] }));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -24,18 +24,18 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' });
 });
-app.route('/servitec/existencia-taller').get(db.getInformeExistenciaRealTaller).post(db.postEntradaEquipoTalller).delete(db.deleteSalidaEquipoTaller);
-app.get('/servitec/equipos-por-cliente/:cliente',db.getInformeEquipoCliente);
-app.get('/servitec/equipos-entregados',db.getInformeEquiposEntregados);
-app.get('/servitec/equipos-entregados-fechas',db.getInformeEquiposEntregadosFechas);
 
+app.get('/servitec/equipos-por-cliente/:cliente', db.getInformeEquipoCliente);
+app.get('/servitec/equipos-entregados', db.getInformeEquiposEntregados);
+app.get('/servitec/equipos-entregados-fechas', db.getInformeEquiposEntregadosFechas);
+
+app.route('/servitec/existencia-taller').get(db.getInformeExistenciaRealTaller).post(db.postEntradaEquipoTalller).delete(db.deleteSalidaEquipoTaller);
 app.route('/servitec/clientes').get(db.getListaClientes).post(db.addCliente);
 app.route('/servitec/estados').get(db.getEstados).post(db.addEstado);
 app.route('/servitec/productos').get(db.getListaProductos).post(db.addProducto);
 app.route('/servitec/tecnicos').get(db.getListaTecnicos).post(db.addTecnico);
 app.route('/servitec/tipo-servicio').get(db.getTipoServicio).post(db.addTipoServicio);
 /*app.get('/servitec/existenciremote: Counting objects: 100% (42a-taller', db.getInformeExistenciaRealTaller);
-app.post('existencia-taller',db.);
 app.get('/users/:id', db.getUserById);
 app.post('/users', db.createUser);
 app.put('/users/:id', db.updateUser);
--- queries.js
+++ queries.js
@@ -3,8 +3,8 @@ const { response, request } = require('express');
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'postgres',
-  host: '192.168.1.69',
-  database: 'serviteccontrol_toner2022',
+  host: 'localhost',
+  database: 'servitecDB',
   password: 'RyuK6699.cu',
   port: 5432,
 });
@@ -20,23 +20,41 @@ const getInformeExistenciaRealTaller = (request, response, next) => {
     }
   });
 };
+
 const postEntradaEquipoTalller = (request, response, next) => {
   console.log(request.body);
-  response.json(request.body);
-  /*pool.query('SELECT public.entrada_producto_taller()',(error, results)=>{
-     if (error) {
-      next(error);
-    } else {
-      response.status(200).json(results.rows);
-    }
-  });*/
+  const { tecnico, idCliente, fecha_de_entrada, tipo_servicio, equipo, modelo, inventario, accesorios, trabajo_a_realizar, observaciones, telefono } = request.body;
+  pool.query(
+    'SELECT public.entrada_producto_taller($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)',
+    [tecnico, idCliente, fecha_de_entrada, tipo_servicio, equipo, modelo, inventario, accesorios, trabajo_a_realizar, observaciones, telefono],
+    (error, results) => {
+      if (error) {
+        next(error);
+      } else {
+        response.status(200);
+      }
+    });
 };
-const deleteSalidaEquipoTaller = (request, response, next) => {
 
+
+const deleteSalidaEquipoTaller = (request, response, next) => {
+  const { id, modelo, trabajo_realizado, situacion, argumento, fecha_salida, nombrePersona_recoge, apellidoPersona_recoge, c_i } = request.body;
+  pool.query('SELECT public.salida_producto_taller()',
+    [id, `${nombrePersona_recoge} ${apellidoPersona_recoge}`, c_i, trabajo_realizado, situacion, argumento, fecha_salida],
+    (error, results) => {
+      if (error) {
+        next(error);
+      } else {
+        response.status(200);
+      }
+    });
 };
 
 //Informes 
 const getInformeEquipoCliente = (request, response, next) => {
+  app.get('/servitec/equipos-por-cliente/:cliente', db.getInformeEquipoCliente);
+  app.get('/servitec/equipos-entregados', db.getInformeEquiposEntregados);
+  app.get('/servitec/equipos-entregados-fechas', db.getInformeEquiposEntregadosFechas);
   const cliente = request.params.cliente;
   pool.query('SELECT * from get_equipos_por_cliente_existencia($1)', [cliente], (error, results) => {
     if (error) {
@@ -61,13 +79,15 @@ const getInformeEquiposEntregadosFechas = (request, response, next) => {
   const fecha1 = request.query.fecha1;
   const fecha2 = request.query.fecha2;
   //console.log(`fecha1: ${fecha1} y fecha2: ${fecha2}`)
-  pool.query('SELECT * from get_equipos_entregados_2013_2015_fechas($1,$2)', [fecha1, fecha2], (error, results) => {
-    if (error) {
-      next(error);
-    } else {
-      response.status(200).json(results.rows);
-    }
-  });
+  pool.query('SELECT * from get_equipos_entregados_2013_2015_fechas($1,$2)',
+    [fecha1, fecha2],
+    (error, results) => {
+      if (error) {
+        next(error);
+      } else {
+        response.status(200).json(results.rows);
+      }
+    });
 };
 
 
@@ -81,8 +101,18 @@ const getListaClientes = (request, response, next) => {
     }
   });
 };
-const addCliente=(request,response,next)=>{
-
+const addCliente = (request, response, next) => {
+  const { idCliente, nombre_cliente, direccion } = request.body;
+  pool.query(
+    'SELECT * from public.add_cliente($1,$2,$3)',
+    [idCliente, nombre_cliente, direccion],
+    (error, results) => {
+      if (error) {
+        next(error);
+      } else {
+        response.status(200);
+      }
+    });
 };
 
 //Visualizar o modificar lista de productos
@@ -96,8 +126,15 @@ const getListaProductos = (request, response, next) => {
   });
 };
 
-const addProducto=(request,response,next)=>{
-  
+const addProducto = (request, response, next) => {
+  const { idProducto, nombre_producto } = request.body;
+  pool.query('SELECT * from add_producto($1)', [nombre_producto], (error, results) => {
+    if (error) {
+      next(error);
+    } else {
+      response.status(200).json(results.rows);
+    }
+  });
 };
 
 //Visualizar o modificar lista de tecnicos
@@ -110,8 +147,15 @@ const getListaTecnicos = (request, response, next) => {
     }
   });
 };
-const addTecnico=(request,response,next)=>{
-  
+const addTecnico = (request, response, next) => {
+  const { idTecnico, nombre_tecnico, cargo, c_i } = request.body;
+  pool.query('SELECT * from add_producto($1,$2,$3)', [nombre_tecnico, cargo, c_i], (error, results) => {
+    if (error) {
+      next(error);
+    } else {
+      response.status(200).json(results.rows);
+    }
+  });
 };
 
 //Visualizar o modificar lista de estados
@@ -125,8 +169,8 @@ const getEstados = (request, response, next) => {
 
   });
 };
-const addEstado=(request,response,next)=>{
-  
+const addEstado = (request, response, next) => {
+
 };
 
 //Visualizar o modificar lista de tipos de servicio
@@ -140,91 +184,37 @@ const getTipoServicio = (request, response, next) => {
 
   });
 };
-const addTipoServicio=(request,response,next)=>{
-  
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query('SELECT * FROM lista_de_tecnicos WHERE idtecnico = $1', [id], (error, results) => {
+const addTipoServicio = (request, response, next) => {
+  const { idServicio, nombre_servicio } = request.body;
+  pool.query('SELECT * from add_producto($1)', [nombre_servicio], (error, results) => {
     if (error) {
-      throw error;
+      next(error);
+    } else {
+      response.status(200).json(results.rows);
     }
-    response.status(200).json(results.rows);
   });
 };
 
-const createUser = (request, response) => {
-  const { name, email } = request.body;
 
-  pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [name, email],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
-    }
-  );
-};
-
-const updateUser = (request, response) => {
-  const id = parseInt(request.params.id);
-  const { name, email } = request.body;
-
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).send(`User modified with ID: ${id}`);
-    }
-  );
-};
-
-const deleteUser = (request, response) => {
-  const id = parseInt(request.params.id);
-
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+const getDescripciones = (request, response, next) => { 
+  pool.query('SELECT * from get_lista_descripciones()', [], (error, results) => {
     if (error) {
-      throw error;
+      next(error);
+    } else {
+      response.status(200).json(results.rows);
     }
-    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+
+const addDescripcion = (request, response, next) => {
+  const { idDesc, descripcion,precioMN,precioUSD,direccion,trabajo_realizado} = request.body;
+  pool.query('SELECT * from add_descripcion($1,$2,$3,$4)', [descripcion,precioMN,precioUSD,trabajo_realizado], (error, results) => {
+    if (error) {
+      next(error);
+    } else {
+      response.status(200).json(results.rows);
+    }
   });
 };
 
@@ -240,9 +230,11 @@ module.exports = {
   getListaProductos,
   getListaTecnicos,
   getTipoServicio,
+  getDescripciones,
   addCliente,
   addEstado,
   addProducto,
   addTecnico,
-  addTipoServicio
+  addTipoServicio,
+  addDescripcion
 };
